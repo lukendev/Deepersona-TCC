b6a57335e8dd9a97995b70bf206de8294a2c629d
Lucas Azevedo <lucas.azevedo2104@gmail.com>
2019-09-27 13:24:17 -0300

    Walk by Pointn Click DONE!
    Just followed the Brackeys tutorial about walk with pointn click (pathfinding), and adapted to a Mixamo example model, it's working good, but it will take too many time for animation artists to make all of that animations that the script is using.

---
+++ Deepersona/Assets/Scripts/Walk_by_PointClick.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Walk_by_PointClick : MonoBehaviour
+{
+    
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    public ThirdPersonCharacter character;
+
+    void Start()
+    {
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // MOVE OUR AGENT
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        if(agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        } else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+
+    }
+}
