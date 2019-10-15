92aa3da966adf58b49fb03fcba85a396a984288c
Lucas Azevedo <lucas.azevedo2104@gmail.com>
2019-10-14 21:44:35 -0300

    Organização e Implementação de Menu
    - Assets do Felipe deletados, dando problema.
    - Organização das pastas.
    - implementação do menu principal com tela de loading.

---
+++ Deepersona_Prototype/Assets/Scripts/Battle/Battle_AnswerStarter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battle_AnswerStarter : MonoBehaviour
+{
+
+    public GameObject answer1;
+    public GameObject answer2;
+    public GameObject answer3;
+
+    public GameObject enemyMoral;
+
+    public GameObject button;
+
+
+    public GameObject player;
+    public GameObject enemy;
+
+    
+
+    public void StartAnswers()
+    {
+        Walk_by_PointClick.canMove = false;
+        player.transform.LookAt(enemy.transform);
+        enemy.transform.LookAt(player.transform);
+
+
+        answer1.SetActive(true);
+        answer2.SetActive(true);
+        answer3.SetActive(true);
+        button.SetActive(false);
+        enemyMoral.SetActive(true);
+    }
+}
---
+++ Deepersona_Prototype/Assets/Scripts/Scene Control/Creditos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creditos : MonoBehaviour {
+
+    private float localposition;
+    public float speed;
+    public float aceleration;
+
+	// Use this for initialization
+	void Start () {
+        localposition = gameObject.transform.position.y;
+        gameObject.transform.position = new Vector3 (gameObject.transform.position.x, -1000, gameObject.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.transform.position.y <= localposition)
+        gameObject.transform.position += new Vector3(0, speed * aceleration, 0);
+	}
+}
