0a779f513331b63288c7e52e8c73c9c99429f5c0
Lucas Azevedo <lucas.azevedo2104@gmail.com>
2019-10-18 13:50:29 -0300

    Indicador do nível de ansiedade implementado

--- Deepersona_Prototype/Assets/Scripts/Battle/Stress_Level.cs
+++ Deepersona_Prototype/Assets/Scripts/Battle/Stress_Level.cs
@@ -6,8 +6,13 @@ public class Stress_Level : MonoBehaviour
 {
 
     public GameObject stress1, stress2, stress3;
-
     public static float stress;
+    public GameObject player;
+    Vector3 moveDir;
+    public float visionRange;
+    public LayerMask OnlyPlayer;
+
+
 
 
     // Start is called before the first frame update
@@ -17,8 +22,40 @@ public class Stress_Level : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        moveDir = transform.forward;
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.forward, out hit, visionRange, OnlyPlayer))
+        {
+            if(hit.collider.CompareTag("Player"))
+            {
+                if(hit.distance < 2)
+                {
+                    stress1.SetActive(true);
+                    stress2.SetActive(false);
+                    stress3.SetActive(false);
+                }
+                else if(hit.distance < 5)
+                {
+                    stress1.SetActive(false);
+                    stress2.SetActive(true);
+                    stress3.SetActive(false);
+                }
+                else if(hit.distance < 9)
+                {
+                    stress1.SetActive(false);
+                    stress2.SetActive(false);
+                    stress3.SetActive(true);
+                }
+                else
+                {
+                    stress1.SetActive(false);
+                    stress2.SetActive(false);
+                    stress3.SetActive(false);
+                }
+            }
+        }
     }
 }
